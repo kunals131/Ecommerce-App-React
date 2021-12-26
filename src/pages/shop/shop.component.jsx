71a084dbb9e79029f6baff80bde0df5ce1ca5d88
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-26 14:10:55 +0530

    Added  Spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,38 +1,50 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import CollectionOverview from '../../components/collections-overview/CollectionOverview.component'
-import CollectionPage from '../collection/Collection.component';
-import { getListOfCollections } from '../../firebase/firebase.utils';
-import { connect } from 'react-redux';
-import { updateCollections } from '../../redux/shop/shop.action';
+import React from "react";
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import CollectionOverview from "../../components/collections-overview/CollectionOverview.component";
+import CollectionPage from "../collection/Collection.component";
+import { getListOfCollections } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shop.action";
+import withSpinner from "../../components/with-spinner/with-spinner.component";
 
-const ShopPage = ({updateCollections, match})=> {
+const CollectionOverViewWithSpinner = withSpinner(CollectionOverview);
+const CollectionPageWithSpinner = withSpinner(CollectionPage);
 
-        
+const ShopPage = ({ updateCollections, match }) => {
+  const [loading, setLoading] = useState(true);
 
-        useEffect(()=>{
-               const getData = async ()=>{
-                       let collections = await getListOfCollections();
-                       updateCollections(collections);
+  useEffect(() => {
+    const getData = async () => {
+      let collections = await getListOfCollections();
+      updateCollections(collections);
+      setLoading(false);
+    };
+    getData();
+  }, []);
 
-               }
-               getData();
-        }, [])
-
-
-
-        return (
-        <div className='shop-page'>
-        <Switch>
-        <Route exact path={`${match.path}`} component={CollectionOverview}/>
-        <Route  exact path={`${match.path}/:category_id`} component={CollectionPage}></Route>
-        </Switch>
-        </div>
-        );
-
-}
+  return (
+    <div className="shop-page">
+      <Switch>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionOverViewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          exact
+          path={`${match.path}/:category_id`}
+          render={(props) => (
+                <CollectionPageWithSpinner isLoading={loading} {...props} />
+              )}
+        ></Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default connect(null, {
-        updateCollections
-})(ShopPage)
+  updateCollections,
+})(ShopPage);
