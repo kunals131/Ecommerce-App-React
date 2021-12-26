7db8cf532338b55a4f0e27ea77fb21093ab2b162
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-26 21:45:24 +0530

    Added Pwa Functionality

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {store, persistor} from './redux/store'
 
 ReactDOM.render(
@@ -20,7 +20,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorkerRegistration.register();
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {Profiler} from 'react'
 import './homepage.style.scss'
 import Directory from '../../components/directory/directory.components'
 import { HomePageContainer } from './homepage.styles'
 const Homepage = ({ history })=>{
     return(
     <HomePageContainer>
+        <Profiler id="directory" onRender={(id,phase,actualDuration)=>{
+            console.log({
+                id,phase,actualDuration
+            })
+        }}>
         <Directory history={history}></Directory>
+        </Profiler>
     </HomePageContainer>
     );
 }
