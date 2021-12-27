609b86680af27cfd4708f615611856594f05399c
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-27 16:23:57 +0530

    Added Reselect to checkout and login fallback

--- src/pages/checkout/Checkout.component.jsx
+++ src/pages/checkout/Checkout.component.jsx
@@ -4,8 +4,19 @@ import { connect } from 'react-redux'
 
 import StripeCheckoutButton from '../../components/stripe-button/StripeButton.component'
 import CheckoutItem from '../../components/checkout-item/CheckoutItem.component'
+import { createStructuredSelector } from 'reselect'
+import { selectCartItems, selectCartTotalAmount } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selector'
 
-const Checkout = ({cartItems, total}) => {
+const Checkout = ({cartItems, total, user}) => {
+
+    if (!user) {
+        return <div className="no-item">
+            <div className="no-item-heading">Please Login before proceeding to  checkout</div>
+            <div className="no-item-img"></div>
+        </div>
+        
+    }
     return (
     <div className="checkout-page">
         <div className="checkout-header">
@@ -35,16 +46,16 @@ const Checkout = ({cartItems, total}) => {
         </div>
 
 {total!==0&&<StripeCheckoutButton price={total}/>}
+
     </div>
     )
 }
 
-const mapStateToProps = (state)=>{
-    let cartItems = state.cart.cartItems;
-    let totalPrice = cartItems.reduce((accumulate,item)=>accumulate+item.quantity*item.price,0);
-    return {
-        cartItems : cartItems,
-        total : totalPrice
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    cartItems : selectCartItems,
+    total : selectCartTotalAmount,
+    user : selectCurrentUser
+});
+
+
 export default connect(mapStateToProps)(Checkout);
