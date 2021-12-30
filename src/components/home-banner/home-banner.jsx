0665e6d5b09768c67e654d27d1329348e0bb1439
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-30 22:51:50 +0530

    Added Favicon

--- src/components/home-banner/home-banner.jsx
+++ src/components/home-banner/home-banner.jsx
@@ -9,17 +9,7 @@ const HomeBanner = () => {
                 <div className="home-banner__left">
                     <h1 className="heading">Winter 21'</h1>
                     <p className="sub-heading">Grad the latest Winter Sale Collection with 30% Off and other special discounts</p>
-                    <div className="buttons">
-                        <div className="button-1 selected">
-                            <div style={{backgroundColor : 'grey'}} className="button-1-inside grey"></div>
-                        </div>
-                        <div className="button-1 second">
-                            <div style={{backgroundColor : 'black'}} className="button-1-inside darkgrey"></div>
-                        </div>
-                        <div className="button-1 second">
-                            <div  style={{backgroundColor : 'rgb(70, 70, 70)'}} className="button-1-inside black"></div>
-                        </div>
-                    </div>
+ 
                     <p className="price">Price : <span>1789$ </span></p>
                     <div className="shop-button-container">
                     <button className="shop-button">Shop Now!</button>
